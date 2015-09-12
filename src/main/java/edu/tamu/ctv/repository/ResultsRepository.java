57e15286727a8aafef047d9b2e2bf6be37b7865d
ashyhanov <ashyhanov@users.noreply.github.com>
2015-09-12 18:05:16 -0500

    dd

--- src/main/java/edu/tamu/ctv/repository/ResultsRepository.java
+++ src/main/java/edu/tamu/ctv/repository/ResultsRepository.java
@@ -2,14 +2,18 @@ package edu.tamu.ctv.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
+import edu.tamu.ctv.entity.Columnheaders;
 import edu.tamu.ctv.entity.Results;
 
 @Repository
 public interface ResultsRepository extends CrudRepository<Results, Long>
 {
-	List<Results> findByProjectsId(Long id);
+	//@Query("from Results as res inner join res.projects project where project.id = :id")
+	List<Results> findByProjectsId(@Param("id")Long id);
 }
