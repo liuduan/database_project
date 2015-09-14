81cf517906e10942d22e83260f0d001a07a75e9c
Viktor <pascal902@gmail.com>
2015-09-14 15:21:49 -0500

    Changes

---
+++ src/main/java/edu/tamu/ctv/entity/enums/Access.java
@@ -0,0 +1,58 @@
+package edu.tamu.ctv.entity.enums;
+
+public enum Access
+{
+	PRIVATE(0, "Private"),
+	PROTECTED(1, "Protected"),
+	REGISTEREDREAD(2, "For registered users (readonly)"),
+	REGISTEREDEDIT(3, "For registered users (allow edit)"),
+	PUBLICREAD(4, "Public (readonly)"),
+	PUBLICEDIT(5, "Public (allow edit)");
+
+	private int id;
+	private String value;
+
+	Access(int id, String value)
+	{
+		this.id = id;
+		this.value = value;
+	}
+
+	public int getId()
+	{
+		return id;
+	}
+	
+	public String getValue()
+	{
+		return value;
+	}
+
+	public static Access parse(String val)
+	{
+		Access access = null;
+		for (Access item : Access.values())
+		{
+			if (item.getValue() == val)
+			{
+				access = item;
+				break;
+			}
+		}
+		return access;
+	}
+	
+	public static Access parse(int id)
+	{
+		Access access = null;
+		for (Access item : Access.values())
+		{
+			if (item.getId() == id)
+			{
+				access = item;
+				break;
+			}
+		}
+		return access;
+	}
+}
---
+++ src/main/java/edu/tamu/ctv/entity/enums/Status.java
@@ -0,0 +1,55 @@
+package edu.tamu.ctv.entity.enums;
+
+public enum Status
+{
+	NEW(0, "New"),
+	INPROCESS(1, "In process"),
+	FINESHED(2, "Fineshed");
+
+	private int id;
+	private String value;
+
+	Status(int id, String value)
+	{
+		this.id = id;
+		this.value = value;
+	}
+
+	public int getId()
+	{
+		return id;
+	}
+	
+	public String getValue()
+	{
+		return value;
+	}
+
+	public static Status parse(String val)
+	{
+		Status status = null;
+		for (Status item : Status.values())
+		{
+			if (item.getValue() == val)
+			{
+				status = item;
+				break;
+			}
+		}
+		return status;
+	}
+	
+	public static Status parse(int id)
+	{
+		Status status = null;
+		for (Status item : Status.values())
+		{
+			if (item.getId() == id)
+			{
+				status = item;
+				break;
+			}
+		}
+		return status;
+	}
+}
