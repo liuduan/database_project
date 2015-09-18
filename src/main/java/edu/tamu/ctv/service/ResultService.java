a83ec19dc8fb9e6b749f41153b74a784de7dde9c
Viktor <pascal902@gmail.com>
2015-09-18 10:20:05 -0500

    Changes

--- src/main/java/edu/tamu/ctv/service/ResultService.java
+++ src/main/java/edu/tamu/ctv/service/ResultService.java
@@ -3,6 +3,8 @@ package edu.tamu.ctv.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import edu.tamu.ctv.repository.ResultsRepository;
+
 
 @Service
 public class ResultService
